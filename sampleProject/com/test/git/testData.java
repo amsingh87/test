0c196b36a06a5b8c970ba7c57b7e91451fb5937d
amsingh87 <aleshsingh87@gmail.com>
2017-05-19 19:53:04 -0500

    Commiting the first change

---
+++ sampleProject/com/test/git/testData.java
@@ -0,0 +1,15 @@
+package com.test.git;
+
+public class testData {
+		
+	void read()
+	{
+		System.out.println("From Read");
+	}
+
+	public void display() {
+		System.out.println("From Display");
+		
+	}
+
+}
---
+++ sampleProject/com/test/git/testGit.java
@@ -0,0 +1,12 @@
+package com.test.git;
+
+public class testGit {
+
+	public static void main(String[] args) {
+		
+		testData td = new testData();
+		
+		td.display();
+
+	}
+}
